038e35868cf855608533a1a1dfb39cf26d3f6eac
CyberPipus <pietro.godoy@aluno.cps.sp.gov.br>
2026-09-09 21:25:20 -0300

    Refatoração de FabricaPainel em artefato para registrar progresso

--- siga-factory/src/siga/FabricaPainel.java
+++ siga-factory/src/siga/FabricaPainel.java
@@ -1,15 +1,19 @@
-package siga;
-
-public class FabricaPainel {
-    public Painel criar(String tipo) {
-        if (tipo.equals("ALUNO")) {
-            return new PainelAluno();
-        } else if (tipo.equals("PROFESSOR")) {
-            return new PainelProfessor();
-        } else if (tipo.equals("COORDENADOR")) {
-            return new PainelCoordenador();
-        } else {
-            throw new IllegalArgumentException("Perfil desconhecido: " + tipo);
-        }
-    }
-}
+// Aqui temos um artefato de nosso progresso, mas não é mais necessário. A fábrica de painéis foi substituída pelo 
+// padrão Factory Method, que é mais flexível e extensível. Portanto, este código foi comentado para indicar que 
+// não será mais utilizado, mas ainda está presente para referência histórica.
+//
+//package siga;
+//
+//public class FabricaPainel {
+//    public Painel criar(String tipo) {
+//        if (tipo.equals("ALUNO")) {
+//            return new PainelAluno();
+//        } else if (tipo.equals("PROFESSOR")) {
+//            return new PainelProfessor();
+//        } else if (tipo.equals("COORDENADOR")) {
+//            return new PainelCoordenador();
+//        } else {
+//            throw new IllegalArgumentException("Perfil desconhecido: " + tipo);
+//        }
+//    }
+//}
